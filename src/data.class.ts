e1b9ac4de25a062e12503e93fe27fcfcdb7d19fe
kobi <kobi.b@inner-active.com>
2018-01-24 12:08:05 +0200

    undefined key strategy

--- src/data.class.ts
+++ src/data.class.ts
@@ -7,7 +7,7 @@ export type Constructor<T = {}> = new (...args: any[]) => T;
 function copy<T>(newValue: Partial<T> = {}): T {
   const changes: any = {}
   for (const p of Object.keys(this)) {
-    changes[p] = (newValue as any)[p] == null ? this[p] : (newValue as any)[p]
+    changes[p] = (newValue as any)[p] === void 0 ? this[p] : (newValue as any)[p]
   }
   return new this.constructor(changes)
 }
