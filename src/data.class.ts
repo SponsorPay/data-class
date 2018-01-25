df33f56ce2c6f8360cfe99304b9e39afc0f52c71
kobi <kobi.b@inner-active.com>
2018-01-25 13:07:06 +0200

    add mutate

--- src/data.class.ts
+++ src/data.class.ts
@@ -1,5 +1,6 @@
 export interface DataClass<T> {
-  copy(newValue?: Partial<T>): T
+  copy(newValue?: Partial<T>): T;
+  mutate(newValue?: Partial<T>): T;
 }
 
 export type Constructor<T = {}> = new (...args: any[]) => T;
@@ -12,9 +13,21 @@ function copy<T>(newValue: Partial<T> = {}): T {
   return new this.constructor(changes)
 }
 
+function mutate<T>(newValue: Partial<T> = {}): T {
+  const changes: any = {}
+  for (const p of Object.keys(this)) {
+    this[p] = (newValue as any)[p] === void 0 ? this[p] : (newValue as any)[p]
+  }
+  return this
+}
+
 export function dataClass<T>(ctor: Constructor<T>) {
   if (ctor.prototype.copy) {
     console.warn(`dataClass: copy is already defined on prototype of provided constructor "${(ctor as any).name}"`)
   }
+  if (ctor.prototype.mutate) {
+    console.warn(`dataClass: mutate is already defined on prototype of provided constructor "${(ctor as any).name}"`)
+  }
   ctor.prototype.copy = copy;
+  ctor.prototype.mutate = mutate;
 }
