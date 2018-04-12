1112468fc613fe45dadf186df7c4b08779c771ed
kobi <kobi.b@inner-active.com>
2018-04-12 19:40:13 +0300

    use "this" return type

--- src/data.class.ts
+++ src/data.class.ts
@@ -1,6 +1,7 @@
-export interface DataClass<T> {
-  copy(newValue?: Partial<T>): T;
-  mutate(newValue?: Partial<T>): T;
+export interface DataClass {
+  copy(newValue?: Partial<this>): this;
+
+  mutate(newValue?: Partial<this>): this;
 }
 
 export type Constructor<T = {}> = new (...args: any[]) => T;
@@ -8,25 +9,30 @@ export type Constructor<T = {}> = new (...args: any[]) => T;
 function copy<T>(newValue: Partial<T> = {}): T {
   const changes: any = {}
   for (const p of Object.keys(this)) {
-    changes[p] = (newValue as any)[p] === void 0 ? this[p] : (newValue as any)[p]
+    const value = (newValue as any)[p]
+    changes[p] = value === void 0 ? this[p] : value
   }
   return new this.constructor(changes)
 }
 
 function mutate<T>(newValue: Partial<T> = {}): T {
   for (const p of Object.keys(this)) {
-    this[p] = (newValue as any)[p] === void 0 ? this[p] : (newValue as any)[p]
+    const value = (newValue as any)[p]
+    if (value !== void 0) {
+      this[p] = value
+    }
   }
   return this
 }
 
 export function dataClass<T>(ctor: Constructor<T>) {
-  if (ctor.prototype.copy) {
+  const ctorProto = ctor.prototype as any
+  if (ctorProto.copy) {
     console.warn(`dataClass: copy is already defined on prototype of provided constructor "${(ctor as any).name}"`)
   }
-  if (ctor.prototype.mutate) {
+  if (ctorProto.mutate) {
     console.warn(`dataClass: mutate is already defined on prototype of provided constructor "${(ctor as any).name}"`)
   }
-  ctor.prototype.copy = copy;
-  ctor.prototype.mutate = mutate;
+  ctorProto.copy = copy;
+  ctorProto.mutate = mutate;
 }
--- test/data.class.test.ts
+++ test/data.class.test.ts
@@ -1,15 +1,46 @@
-import {DataClass, dataClass} from "../src/data.class"
+import {DataClass, dataClass} from "../src"
 import chai = require("chai")
+
 chai.should();
 
-interface User extends DataClass<User> {
+interface UserParams {
   name: string
   age: number
 }
 
+interface User extends UserParams, DataClass {
+
+}
+
 class User {
-  constructor({name = "Guest", age = 0}: Partial<User> = {}) {
-    Object.assign(this, {name, age});
+  static EMPTY = new User({
+    age: 0,
+    name: "Guest",
+  })
+
+  constructor(params: UserParams) {
+    Object.assign(this, params)
+  }
+}
+
+interface AwesomeUserParams extends UserParams {
+  awesomeness: number;
+}
+
+interface AwesomeUser extends AwesomeUserParams, User {
+
+}
+
+class AwesomeUser extends User {
+  static EMPTY = new AwesomeUser({
+    age: 0,
+    name: "Awesome Guest",
+    awesomeness: 10
+  })
+
+  constructor(params: AwesomeUserParams) {
+    super(params)
+    Object.assign(this, params)
   }
 }
 
@@ -18,12 +49,30 @@ dataClass(User);
 describe("data class", function () {
   this.timeout(1000);
   it("should copy", () => {
-    const user = new User();
+    const user = User.EMPTY
     user.copy({name: "Ben"}).name.should.eq("Ben");
     user.copy({name: "Ben"}).age.should.eq(0);
     user.copy({name: "Ben", age: 29}).age.should.eq(29);
     (user === user.copy()).should.eq(false);
     user.name.should.eq("Guest");
     user.age.should.eq(0);
+    user.copy({name: "Ben"}).should.be.instanceOf(User)
   });
+
+  it("should mutate", () => {
+    const user = User.EMPTY.copy()
+    const mutatedUser = user.mutate({name: "Ben"});
+    (mutatedUser === user).should.be.true;
+    mutatedUser.name.should.eq("Ben");
+  })
+
+  it("infers copy correctly on sub-class", () => {
+    const a = AwesomeUser.EMPTY
+    const upgraded = a.copy({
+      awesomeness: a.awesomeness + 1
+    })
+    a.awesomeness.should.eq(10)
+    upgraded.awesomeness.should.eq(11)
+    upgraded.should.be.instanceOf(AwesomeUser)
+  })
 });
